cf4db4254b08aad143673f0f80d51e81af2afaf2
Eric Di Re <eric.di.re@gmail.com>
2023-03-30 15:04:45 -0700

    Large update to hit various dbs, run different on local machine, split out main code and more.

--- app/app.py
+++ app/app.py
@@ -1,171 +1,29 @@
+#%% Template Imports
 
 import os
 import dlogging
 from demail.gmail import SendEmail
 
 
-package_name = 'selenium_to_sql'
-logger = dlogging.NewLogger(__file__, use_cd=True, backup_count=0)
+package_name = os.getenv('package_name')
+logger = dlogging.NewLogger(__file__, use_cd=True)
+logger.info('Beginning package')
 
-
-#%% begin try
 
 try:
 
-    error_string = ''
-    logger.info('Beginning package')
+    import main
 
+    logger.info('Done! No problems.\n')
 
-    #%% import libraries
-
-    logger.info('Import Libraries')
-
-    import pandas as pd
-    import dwebdriver
-    import ddb
-    from ddb.sql import SQL
-    from time import sleep
-    
-    directory = os.path.dirname(os.path.realpath(__file__))
-    directory_download = os.path.join(directory, 'downloads')
-    
-    
-    #%% odbc connector
-    
-    logger.info('Create ODBC connector')
-    
-    odbc = SQL(db=os.getenv('sql_db'), server=os.getenv('sql_server'), local_cred='no', uid=os.getenv('sql_uid'), pwd=os.getenv('sql_pwd'))
-    
-    
-    #%% odbc env variables
-    
-    logger.info('Gather env dataframe')
-    
-    df_env = odbc.read('select * from eggy.tblScrapeDirector_EnvList')
-    for s in range(df_env.shape[0]):
-        os.environ[df_env.at[s, 'env_name']] = df_env.at[s, 'env_value']
-        
-    package_name = os.getenv('package_name')
-    
-    
-    #%% odbc scrape director
-    
-    logger.info('Gather scraping dataframe')
-    
-    sql_scrape_director_tbl = os.getenv('sql_scrape_director_tbl')
-    df_scrape = odbc.read(f'select * from {sql_scrape_director_tbl} order by sort_by')
-    
-   
-    #%% Clean Workspace
-
-    logger.info('Clean Workspace')
-    
-    for file in os.listdir(directory_download):
-        filepath = os.path.join(directory_download, file)
-        os.remove(filepath)
-    
-    
-    #%% sql load
-    
-    logger.info('Define Functions')
-    
-    sql_schema_landing = os.getenv('sql_schema_landing')
-    
-    def SQLLoad(**kwargs):
-        for file in os.listdir(directory_download):
-            filepath = os.path.join(directory_download, file)
-            filename, fileext = os.path.splitext(file)
-            load_file = False
-            if fileext == '.xlsx':
-                df = pd.read_excel(filepath, **kwargs)
-                load_file = True
-            if load_file == True:
-                df = ddb.clean(df, rowloadtime=True, drop_cols=False)
-                sql_tbl = 'tbl' + ddb.clean_string(filename)
-                odbc.to_sql(df, sql_tbl, schema=sql_schema_landing, if_exists='replace', index=False, extras=True)
-            os.remove(filepath)
-    
-    
-    #%% webscrape
-    
-    logger.info('Begin webscraping')
-    
-    with dwebdriver.ChromeDriver(download_directory=directory_download
-                                  , no_sandbox=True
-                                  , window_size='1920,1080'
-                                  ) as driver:
-    
-        for i in range(df_scrape.shape[0]):
-            df_temp = df_scrape[df_scrape.index == i].copy()
-            logger.info(f"webscrape - {df_temp.at[i, 'descrip']}")
-            if df_temp.at[i, 'command'] == 'url':
-                try:
-                    url = df_temp.at[i, 'command_value']
-                    post_time_delay = df_temp.at[i, 'post_time_delay']
-                    driver.get(url)
-                    sleep(post_time_delay)
-                except Exception as e:
-                    e = str(e)
-                    error_string += e + '\n\n'
-                    logger.critical(e)
-            elif df_temp.at[i, 'command'] == 'loadfile':
-                try:
-                    load_args = eval(df_temp.at[i, 'command_value'])
-                    SQLLoad(**load_args)
-                except Exception as e:
-                    e = str(e)
-                    error_string += e + '\n\n'
-                    logger.critical(e)
-            elif df_temp.at[i, 'command'] == 'runproc':
-                try:
-                    run_procs = df_temp.at[i, 'command_value']
-                    for proc in run_procs.split(','):
-                        sql = f'EXEC {proc}'
-                        odbc.run(sql)
-                except Exception as e:
-                    e = str(e)
-                    error_string += e + '\n\n'
-                    logger.critical(e)
-            else:
-                try:
-                    driver.process_df(df_temp, odbc_db=odbc)
-                except Exception as e:
-                    e = str(e)
-                    error_string += e + '\n\n'
-                    logger.critical(e)
-                
-    
-    logger.info('Webscrape complete')
-    
-    
-    #%% Success Email
-    
-    if error_string == '':
-        sql = f"EXEC eggy.stpPythonLogging @package_name = '{package_name}', @is_success = 1, @error_descrip = NULL"
-        odbc.run(sql)
-        
-        SendEmail(os.getenv('email_success'), subject=f'{package_name} Complete', body=f'Data successfully imported for {package_name}!', user=os.getenv('yagmail_uid'), password=os.getenv('yagmail_pwd'))
-    
-        logger.info('Done! No problems.\n')
-        
-    else:
-        raise Exception("Errors occurred loading files\n\n")
-    
-    
-#%% Error Handling
 
 except Exception as e:
-    e = str(e) + error_string
+    e = str(e)
     logger.critical(f'{e}\n', exc_info=True)
-    to_email_addresses = os.getenv('email_fail')
-    body = f'Error running package {package_name}\nError message:\n{e}'
-    SendEmail(to_email_addresses=to_email_addresses
+    SendEmail(to_email_addresses=os.getenv('email_fail')
                         , subject=f'Python Error - {package_name}'
-                        , body=body
+                        , body=e
                         , attach_file_address=logger.handlers[0].baseFilename
-                        , user=os.getenv('yagmail_uid')
-                        , password=os.getenv('yagmail_pwd')
+                        , user=os.getenv('email_uid')
+                        , password=os.getenv('email_pwd')
                         )
-
-    sql = f"EXEC eggy.stpPythonLogging @package_name = '{package_name}', @is_success = 0, @error_descrip = '{e}'"
-    odbc.run(sql)
---
+++ app/main.py
@@ -0,0 +1,213 @@
+#%% Local Code Only
+
+local_computer = 'powerhouse2'
+import socket
+host_name = socket.gethostname()
+if host_name == local_computer:
+    filepath_scrapedirector = './verde_apx/scrape_director.csv'
+    filepath_scrapeenv = './verde_apx/env_list.csv'
+    from dotenv import load_dotenv
+    load_dotenv('./verde_apx/.env', override=True)
+
+
+#%% Imports
+import os
+import dlogging
+from demail.gmail import SendEmail
+
+
+package_name = os.getenv('package_name')
+logger = dlogging.NewLogger(__file__, use_cd=True, backup_count=0)
+
+error_string = ''
+logger.info('Beginning package')
+
+
+#%% import libraries
+
+logger.info('Import Libraries')
+
+import pandas as pd
+import dwebdriver
+import ddb
+
+directory = os.path.dirname(os.path.realpath(__file__))
+directory_download = os.path.join(directory, 'downloads')
+
+
+#%% engine connector
+
+logger.info('Create engine connector')
+
+engine_type = os.getenv('engine')
+if engine_type == 'sql':
+    from ddb.sql import SQL
+    engine_vars = {
+        'db':os.getenv('sql_db'),
+        'server':os.getenv('sql_server'),
+        'uid':os.getenv('sql_uid'),
+        'pwd':os.getenv('sql_pwd'),
+    }
+elif engine_type == 'mysql':
+    from ddb.mysql import SQL
+    engine_vars = {
+        'db':os.getenv('mysql_db'),
+        'server':os.getenv('mysql_server'),
+        'uid':os.getenv('mysql_uid'),
+        'pwd':os.getenv('mysql_pwd'),
+    }
+elif engine_type == 'bigquery':
+    from ddb.bigquery import SQL
+    engine_vars = {
+        'credentials_filepath':os.getenv('bigquery_cred')
+    }
+    
+engine = SQL(**engine_vars)
+
+
+#%% engine env variables
+
+logger.info('Gather env dataframe')
+if host_name == local_computer:
+    df_env = pd.read_csv(filepath_scrapeenv)
+else:
+    engine_env_tbl = os.getenv('engine_env_tbl')
+    if engine_env_tbl != None:
+        df_env = engine.read(f'select * from {engine_env_tbl}')
+
+if 'df_env' in locals():
+    for s in range(df_env.shape[0]):
+        os.environ[df_env.at[s, 'env_name']] = df_env.at[s, 'env_value']
+
+
+#%% engine scrape director
+
+logger.info('Gather scraping dataframe')
+
+if host_name == local_computer:
+    df_scrape = pd.read_csv(filepath_scrapedirector)
+    df_scrape = df_scrape[pd.isna(df_scrape['command'])==False]
+    df_scrape.reset_index(drop=True, inplace=True)
+else:
+    sql_scrape_director_tbl = os.getenv('sql_scrape_director_tbl')
+    df_scrape = engine.read(f'select * from {sql_scrape_director_tbl} order by sort_by')
+
+
+#%% Clean Workspace
+
+logger.info('Clean Workspace')
+
+for file in os.listdir(directory_download):
+    filepath = os.path.join(directory_download, file)
+    os.remove(filepath)
+
+
+#%% sql load
+
+logger.info('Define Functions')
+
+def SQLLoad(**kwargs):
+    for file in os.listdir(directory_download):
+        logger.info(file)
+        filepath = os.path.join(directory_download, file)
+        filename, fileext = os.path.splitext(file)
+        load_file = False
+
+        if fileext == '.xlsx':
+            df = pd.read_excel(filepath, **kwargs['load_args'])
+            load_file = True
+        elif fileext == '.csv':
+            df = pd.read_csv(filepath)
+            load_file = True
+
+        if load_file == True:
+            if 'clean' in kwargs:
+                if kwargs['clean'] == 0:
+                    pass
+            else:
+                df = ddb.clean(df, rowloadtime=True, drop_cols=False)
+            if 'sql_tbl' not in kwargs:
+                sql_tbl = 'tbl' + ddb.clean_string(filename)
+                sql_schema_landing = 'stage'
+            elif '.' in kwargs['sql_tbl']:
+                sql_schema_landing, sql_tbl = kwargs['sql_tbl'].split('.')
+            else:
+                sql_tbl = kwargs['sql_tbl']
+                sql_schema_landing = None
+            engine.to_sql(df, sql_tbl, schema=sql_schema_landing, if_exists='replace', index=False, extras=True)
+        os.remove(filepath)
+
+
+#%% webscrape
+
+logger.info('Begin webscraping')
+
+with dwebdriver.ChromeDriver(download_directory=directory_download
+                                , no_sandbox=True
+                                , window_size='1920,1080'
+                                ) as driver:
+
+    for i in range(df_scrape.shape[0]):
+        df_temp = df_scrape[df_scrape.index == i].copy()
+        logger.info(f"webscrape - {df_temp.at[i, 'descrip']}")
+        if df_temp.at[i, 'command'] == 'loadfile':
+            try:
+                command_value_type = df_temp.at[i, 'command_value_type']
+                command_value = df_temp.at[i, 'command_value']
+                if command_value_type == 'python':
+                    _locals = {}
+                    exec(command_value, None, _locals)
+                SQLLoad(**_locals)
+            except Exception as e:
+                e = str(e)
+                error_string += e + '\n\n'
+                logger.critical(e)
+        elif df_temp.at[i, 'command'] == 'runproc':
+            try:
+                run_procs = df_temp.at[i, 'command_value']
+                for proc in run_procs.split(','):
+                    engine.run(proc)
+            except Exception as e:
+                e = str(e)
+                error_string += e + '\n\n'
+                logger.critical(e)
+        else:
+            try:
+                driver.process_df(df_temp, odbc_db=engine)
+            except Exception as e:
+                e = str(e)
+                error_string += e + '\n\n'
+                logger.critical(e)
+            
+
+logger.info('Webscrape complete')
+
+
+#%% Success Email
+
+if error_string == '':
+    logger.info('Record success in SQL and send email')
+
+    sql = f"INSERT INTO {os.getenv('error_logging_tbl')} (package_name, is_success, error_descrip)" \
+        f"\nVALUES ('{package_name}', 1, NULL)"
+    engine.run(sql)
+    
+    sql_last_sale = "SELECT MAX(sr.SignedDate) as max_sale FROM dbo.tblSalesReport sr"
+    df_last_sale = engine.read(sql_last_sale)
+    str_last_sale = f'Last sale date loaded to SQL: {str(df_last_sale.iat[0, 0])}'
+
+    SendEmail(to_email_addresses=os.getenv('email_success'),
+              subject=f'{package_name} Complete',
+              body=f'Data successfully imported for {package_name}!\n\n{str_last_sale}',
+              user=os.getenv('email_uid'),
+              password=os.getenv('email_pwd'))
+    
+else:
+    logger.info('Record errors in SQL and raise Exception')
+
+    e = error_string.replace("'", "")
+    sql = f"INSERT INTO {os.getenv('error_logging_tbl')} (package_name, is_success, error_descrip)" \
+        f"\nVALUES ('{package_name}', 0, '{e}')"
+    engine.run(sql)
+
+    raise Exception('Errors occured during processing!\n\n' + error_string)
